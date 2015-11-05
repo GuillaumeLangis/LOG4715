a17d94b51af7472f74e32e9edb784178b5382940
GL-THINKPAD\Guillaume <Langis.Guillaume@gmail.com>
2015-11-05 18:05:23 -0500

    Commit initial git

---
+++ Jeu de course/Assets/Cadriciel/Scripts/CarUserControlMP.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CarController))]
+public class CarUserControlMP : MonoBehaviour
+{
+	private CarController car;  // the car controller we want to use
+
+	[SerializeField]
+	private string vertical = "Vertical";
+
+	[SerializeField]
+	private string horizontal = "Horizontal";
+
+    bool jump = false;
+	
+	void Awake ()
+	{
+		// get the car controller
+		car = GetComponent<CarController>();
+	}
+
+    void Update()
+    {
+        if (CrossPlatformInput.GetButtonDown("Fire1"))
+        {
+            car.LaunchProjectile();
+        }
+
+        if (!jump)
+            jump = CrossPlatformInput.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+	{
+		// pass the input to the car!
+		#if CROSS_PLATFORM_INPUT
+		float h = CrossPlatformInput.GetAxis(horizontal);
+		float v = CrossPlatformInput.GetAxis(vertical);
+        car.useNitro = CrossPlatformInput.GetButton("Fire2");
+        if (jump)
+        {
+            car.Jump();
+            jump = false;
+        }
+        #else
+		float h = Input.GetAxis(horizontal);
+		float v = Input.GetAxis(vertical);
+        #endif
+        car.Move(h,v);
+	}
+}
---
+++ Jeu de course/Assets/Sample Assets/PlayerUI.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerUI : MonoBehaviour {
+
+    public CarController controller;
+
+	[SerializeField] Image speedFill;
+    [SerializeField] Image hpFill;
+    [SerializeField] Text speedValue;
+    [SerializeField] Image nitroFill;
+    [SerializeField] Text projectileInfo;
+    [SerializeField] Text stylePointsValue;
+    [SerializeField] Text positionInRaceValue;
+
+    // Direction panels are only active one at a time, must be in the right order in the array
+    [SerializeField] Text[] directionPanels = new Text[4];
+    public enum ESuggestedDirection
+    {
+        Forward,
+        Right,
+        Left,
+        Back,
+        None
+    }
+    ESuggestedDirection _direction;
+    public ESuggestedDirection direction
+    {
+        get { return _direction; }
+        set
+        {
+            _direction = value;
+            for (int i = 0; i < directionPanels.Length; i++)
+            {
+                directionPanels[i].enabled = i == (int)value;
+            }
+        }
+    }
+
+    void SetProjectileInfo(string info)
+    {
+        projectileInfo.text = info;
+    }
+    
+
+    void Awake()
+    {
+        direction = ESuggestedDirection.None;
+    }
+
+    void FixedUpdate()
+    {
+        if (controller)
+        {
+            speedFill.fillAmount = controller.CurrentSpeed / controller.MaxSpeed;
+            speedValue.text = Mathf.RoundToInt(controller.CurrentSpeed).ToString() + " km/h";
+            nitroFill.fillAmount = controller.nitroLeft;
+            stylePointsValue.text = controller.stylePoints.ToString() + " points";
+            hpFill.fillAmount = controller.health;
+            positionInRaceValue.text = (controller.checkpointManager.GetPositionInRace(controller) + 1).ToString();
+
+            SetProjectileInfo(controller.projectilePrefab ? controller.projectilePrefab.name : "No projectile");
+        }
+    }
+}
